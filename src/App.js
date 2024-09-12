01087e04d17947b38289884f6e9128591a59482a
Sughrabia <sughrabia@gmail.com>
2024-09-12 11:50:27 +0500

    new update

--- src/App.js
+++ src/App.js
@@ -12,38 +12,32 @@ import Company from './components/Company/Company';
 import About from './components/About/About';
 import Contactus from './components/Contactus/Contactus';
 import Office from './components/Office/Office';
-import Women_banner from './components/assets/Women_banner.png'
+import Women_banner from './components/assets/Women_banner.png';
+import Men_banner from './components/assets/Men_banner.png';
+import Kids_banner from './components/assets/Kids_banner.png';
+import Login from './pages/login';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path='/'  element={<Shop/>}/>
-        <Route path='/Men'  element={<ShopCategory  category="Men"/>}/>
-        <Route path='/Women'  element={<ShopCategory banner={Women_banner} category="Women"/>}/>
-        <Route path='/Kid'  element={<ShopCategory category="kid"/>}/>
-        <Route>
-          <Route path='/Product' element={<Product/>} />
-          <Route path=':ProductID' element={<Product/>}/>
-        </Route>
-        <Route path='/Cart' element={<Cart/>}/> 
-        <Route path='/login' element={<LoginSignup/>}/>
-        
-      </Routes>
-     
-      <Routes>
-        <Route path='/company' element={<Company/>}/>
-        <Route path='/aboutus' element={<About/>}/>
-        <Route path='/contactus' element={<Contactus/>}/>
-        <Route path='/office' element={<Office/>}/>
-        <Route path='/product' element={<Product/>}/>
-      </Routes>
-      <Footer/>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/Men' element={<ShopCategory banner={Men_banner} category="Men" />} />
+          <Route path='/Women' element={<ShopCategory banner={Women_banner} category="Women" />} />
+          <Route path='/Kid' element={<ShopCategory banner={Kids_banner} category="kid" />} />
+          <Route path='/Product/:ProductID' element={<Product />} />
+          <Route path='/Cart' element={<Cart />} />
+          <Route path='/Loginsignup' element={<LoginSignup />} />
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/Company' element={<Company />} />
+          <Route path='/AboutUs' element={<About />} />
+          <Route path='/ContactUs' element={<Contactus />} />
+          <Route path='/Office' element={<Office />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
-
-      
     </div>
   );
 }
